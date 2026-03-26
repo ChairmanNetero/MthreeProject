fb4308d895d8e98f5d805f1fb515415341636e8a
Netero <m_benab@live.concordia.ca>
2026-03-26 11:46:46 -0400

    final changes 3

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT", 3306) # Added DB_PORT with a default fallback
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,10 @@
 import mysql.connector
 from config import Config
 
-
 def get_db_connection():
     connection = mysql.connector.connect(
         host=Config.DB_HOST,
+        port=Config.DB_PORT, # Added the port argument here
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
         database=Config.DB_NAME
